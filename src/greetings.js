9f561df8871ed243ed7f2e8cb123ef41d8a4c055
SeonhyeY <syoon330@gmail.com>
2023-08-19 22:52:33 -0600

    Different components are shown depending on user register the name or not

--- src/greetings.js
+++ src/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector('.login-form');
 const loginInput = document.querySelector('.login-form input');
 const greeting = document.querySelector('.greeting');
+const logoutComponents = document.querySelectorAll('.logout-component');
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'username';
@@ -10,11 +11,16 @@ const savedUserName = localStorage.getItem(USERNAME_KEY);
 if (savedUserName === null) {
   // show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  greeting.classList.add(HIDDEN_CLASSNAME);
+  logoutComponents.forEach((component) =>
+    component.classList.add(HIDDEN_CLASSNAME)
+  );
   loginForm.addEventListener('submit', onLoginSubmit);
 } else {
   // show the greetings
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  logoutComponents.forEach((component) =>
+    component.classList.remove(HIDDEN_CLASSNAME)
+  );
   showGreeting(savedUserName);
 }
 
@@ -25,10 +31,12 @@ function onLoginSubmit(event) {
   localStorage.setItem(USERNAME_KEY, userName);
 
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  logoutComponents.forEach((component) =>
+    component.classList.remove(HIDDEN_CLASSNAME)
+  );
   showGreeting(userName);
 }
 
 function showGreeting(userName) {
   greeting.innerText = `Hello ${userName}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
