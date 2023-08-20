cdf90f3f224b67e2e70fc2e10fa54f165596433a
SeonhyeY <syoon330@gmail.com>
2023-08-19 21:29:31 -0600

    Show temperature, weather, and city based on user's location

---
+++ src/API_KEY.js
@@ -0,0 +1,1 @@
+var API_KEY = '19aea4d22ae4d6eb87c26e3bb8e9bf09';
---
+++ src/geolocation.js
@@ -0,0 +1,18 @@
+function onGeoOk(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector('.geolocation__weather');
+      const city = document.querySelector('.geolocation__location');
+      weather.innerHTML = `${data.main.temp}&#8451 / ${data.weather[0].main}, `;
+      city.innerText = data.name;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
